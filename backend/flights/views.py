0e23451b42813a62014492d70bbdab11a2a14d2e
Gudala Padma Pranitha <padmap0502@gmail.com>
2026-05-24 18:32:55 +0530

    Add files via upload

--- backend/flights/views.py
+++ backend/flights/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Flight
 
-def search_flights(Request):
+def search_flights(request):
     source = request.GET.get('source')
     destination = request.GET.get('destination')
 
